6d5dbcf767f3ea85269d1ae8933e179677334753
cky <910470585@qq.com>
2022-07-07 20:40:31 +0800

    url

--- api/proxy.ts
+++ api/proxy.ts
@@ -1,21 +1,23 @@
-import {request} from 'request'
+const { createProxyMiddleware } = require('http-proxy-middleware')
 
-module.exports = (req:any, res:any) => {
-  // proxy middleware options
-  let prefix = "/api"
-  if (!req.url.startsWith(prefix)) {
-    return;
+
+module.exports = (req, res) => {
+  let target = ''
+
+  // 代理目标地址
+  // xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
+  if (req.url.startsWith('/api')) {
+  // 这里填目标地址
+    target = 'https://netease-cloud-music-api-theta-nine-20.vercel.app'
   }
-  let target = "https://netease-cloud-music-api-theta-nine-20.vercel.app" + req.url.substring(prefix.length);
-
-  var options = {
-    'method': 'GET',
-    'url': target,
-  };
-  request(options, function (error:any, response:any) {
-    if (error) throw new Error(error);
-    res.writeHead(200, {"Content-Type": "application/json"});
-    res.write(response.body);
-    res.end();
-  });
+  // 创建代理对象并转发请求
+  createProxyMiddleware({
+    target,
+    changeOrigin: true,
+    pathRewrite: {
+      // 通过路径重写，去除请求路径中的 `/api`
+      // 例如 /api/user/login 将被转发到 http://target/user/login
+      '^/api/': '/'
+    }
+  })(req, res)
 }
