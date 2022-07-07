88a5fa977089af71166fd751ae25341b5bc22ba1
cky <910470585@qq.com>
2022-07-07 19:56:15 +0800

    vercel proxy

---
+++ src/service/proxy.ts
@@ -0,0 +1,24 @@
+const request = require('request');
+
+module.exports = (req:any, res:any) => {
+  // proxy middleware options
+  let prefix = "/api"
+  if (!req.url.startsWith(prefix)) {
+    return;
+  }
+  let target = "https://netease-cloud-music-api-theta-nine-20.vercel.app" + req.url.substring(prefix.length);
+
+  var options = {
+    'method': 'GET',
+    'url': target,
+    'headers': {
+      'Authorization': res.headers['authorization']
+    }
+  };
+  request(options, function (error:any, response:any) {
+    if (error) throw new Error(error);
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.write(response.body);
+    res.end();
+  });
+}
