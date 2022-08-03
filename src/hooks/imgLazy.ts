2bb4479fa4b5e7605a95e0d9f2db772b120d3267
cky <910470585@qq.com>
2022-08-03 10:33:23 +0800

    加载图片

--- src/hooks/imgLazy.ts
+++ src/hooks/imgLazy.ts
@@ -1,4 +1,4 @@
-
+import imgUrl from '@/assets/img/img-loading.gif'
 // 全局指令
 export default  (app:any) => {
     // 图片懒加载指令
@@ -16,7 +16,7 @@ export default  (app:any) => {
                 //图片加载完成后
                 el.src = img.src;
             }
-            el.src = 'src/assets/img/img-loading.gif' // 进入视图后，加载图片
+            el.src = imgUrl // 进入视图后，加载图片
           }
         }, {
           threshold: 0.01 // 当图片img元素占比视图0.01时 el.src = '@/assets/img/img-loading.png'
